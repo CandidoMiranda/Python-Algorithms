fe64e9dcf804cabd66f98616dd3e74a6cb8c53f2
cmiranda <candidoguilhermefbm@gmail.com>
2023-11-05 09:03:01 -0300

    fix binarysearch_recursive.py

--- binary search/binaryserach_recursive.py
+++ binary search/binaryserach_recursive.py
@@ -1,16 +1,17 @@
 def binary_search(list, item, low, hight):
-    if len(list) == 1:
-        return guess if item == guess else None
     middle = ((low + hight) // 2)
     guess = list[middle]
+    if low >= hight:
+        return middle if item == guess else None
     if guess == item:
         return middle
     else:
-        return binary_search(list, item, low=low, hight=middle-1) if guess > item else binary_search(list, item, low=middle + 1, hight=hight)
+        return binary_search(list, item, low, middle-1) if guess > item else binary_search(list, item, middle + 1, hight)
 
 example_list = [1, 3, 4, 5, 7, 9, 10, 11]
 
+print(binary_search(example_list, 1, 0, len(example_list) - 1))
 print(binary_search(example_list, 7, 0, len(example_list) - 1))
-print(binary_search(example_list, 2, 0, len(example_list) - 1))
+print(binary_search(example_list, 11, 0, len(example_list) - 1))
 print(binary_search(example_list ,-2, 0, len(example_list) - 1))
 print(binary_search(example_list ,12, 0, len(example_list) - 1))
